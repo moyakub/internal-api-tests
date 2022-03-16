97766fc1caff06cca8216fa2ad360d3a3b535cca
Mohammad Yakub <yakubwpg@gmail.com>
2022-03-15 23:11:44 -0500

    updated test_get_all_products test scenario and added test_update_a_product scenario

--- tests/packages/product.py
+++ tests/packages/product.py
@@ -5,9 +5,18 @@ from . import config_shop_environment as cse
 
 
 def get_all_products():
-  r = requests.get(cse.url,headers=cse.headers)
+  r = requests.get(cse.get_products_url,headers=cse.headers)
   product_dict = r.json()
   return product_dict
+
+
+
+def update_a_product(payload,bold_product_id):
+  url = cse.update_a_product_url + bold_product_id
+  r = requests.put(url,headers=cse.headers, data=payload)
+  product_dict = r.json()
+  return product_dict  
+
 
 
   
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,10 +1,61 @@
+import json
 from packages import product
+from datetime import datetime
+from dateutil import parser
 #import custom_packages.product
 
 #test get all products endpoint
 def test_get_all_products():
+
   product_dict = product.get_all_products()
-  assert product_dict["total"] == 18
+  products_returned = False
+
+  if product_dict["total"]:
+    products_returned = True
+
+  assert products_returned == True
+
+#test update a product endpoint
+def test_update_a_product():
+  new_product_name = "Name-Updated-from-mo-mac"
+  
+  #Extract date and hour from current datetime object
+  current_date_time_utc = datetime.utcnow() 
+  current_date_utc = current_date_time_utc.date()
+  current_hour_utc = current_date_time_utc.hour
+  
+  payload =   { 
+	
+    "product" : {
+        "name": new_product_name,
+        "description": "Description-Updated"
+    }
+	
+  }
+  payload = json.dumps(payload) 
+  bold_product_id = "2027011"
+  product_dict = product.update_a_product(payload,bold_product_id)
+  
+  updated_at = product_dict["product"]["updated_at"]
+
+  #convert ISO 8601 string to a datetime object
+  updated_at_date_time = parser.parse(updated_at)
+  updated_at_date = updated_at_date_time.date()
+  updated_at_hour = updated_at_date_time.hour 
+  
+  #just to debug updated at dates and times. This code is not for test
+  #print(f'Current Date: {current_date_utc} updated date: {updated_at_date} Current hour{current_hour_utc} updated hour {updated_at_hour}')
+
+  #if update product endpoint worked fine then current date and hour should be same as updated_at date and hour.
+  assert current_date_utc == updated_at_date and current_hour_utc == updated_at_hour
+
+
+
+
+
+
+
+
   
 
 
